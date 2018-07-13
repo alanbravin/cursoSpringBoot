14539d368bab7bb137c8e02c06108c2ceaee6b39
Alan <alan.resender@gmail.com>
2018-07-13 18:20:58 -0300

    Mapeamento de Cliente, Pagamentos e Pedido 1 e 2 de 3

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -38,6 +38,8 @@ public class Cliente implements Serializable {
 	@OneToMany(mappedBy = "cliente")
 	private List<Endereco> enderecos = new ArrayList<>();
 
+	private List<Pedido> pedidos = new ArrayList<>();
+	
 	public Cliente(Integer id, String nome, String email, String cpfOuCnpj, TipoCliente tipoCliente) {
 		super();
 		this.id = id;
@@ -97,6 +99,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
--- src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/nelioalves/cursomc/domain/PagamentoComBoleto.java
@@ -1,5 +1,43 @@
 package com.nelioalves.cursomc.domain;
 
-public class PagamentoComBoleto extends Pagamento {
+import java.util.Date;
 
+import com.nelioalves.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComBoleto extends Pagamento {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+		
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estadoPagamento, Pedido pedido,
+			Date dataVencimento, Date dataPagamento) {
+		super(id, estadoPagamento, pedido);
+		this.dataVencimento = dataVencimento;
+		this.dataPagamento = dataPagamento;
+		
+	}
+	
+	
 }
